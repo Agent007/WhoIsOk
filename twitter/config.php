6754a5cf519558336a7205fb4ef407b738358f5b
J <sha0lin@alumni.carnegiemellon.edu>
2010-12-20 13:31:00 -0500

    committing what was demonstrated at the end of Random Hacks of Kindness (Atlanta) on Sunday, December 5, 2010

---
+++ twitter/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', '3qSTRmyPJB2AaoTDWO4w');
+define('CONSUMER_SECRET', 'r7PKBV1eMcaj7Oz0oBclLCjVpPPs4Iqi9OATbXeA5w');
+define('OAUTH_CALLBACK', 'https://academic-php.cc.gatech.edu/groups/cs4400_group22/WhoIsOk/callback.php');
